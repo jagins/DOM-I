76be8bac43a9a1a18e5aa1f224c45a5626626b07
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-02 15:40:41 -0600

    added in webpage elements with js

--- js/index.js
+++ js/index.js
@@ -37,6 +37,54 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
+
+//nav text
+let navText = document.querySelectorAll('a');
+
+navText.forEach((item, index) =>
+  {
+    item.textContent = siteContent["nav"][`nav-item-${index}`];
+  })
+
+//logo image
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//dom is awesome text
+let headerText = document.querySelector('h1');
+headerText.textContent = siteContent.cta.h1;
+
+//get started button text
+let getStartedButton = document.querySelector('button');
+getStartedButton.textContent = siteContent.cta.button;
+
+//matrix image
+let ctaImage = document.getElementById('cta-img');
+ctaImage.src = siteContent.cta["img-src"];
+
+
+//middle image accent
+let middleImage = document.getElementById('middle-img');
+middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+//h4 text
+let h4Text = document.querySelectorAll('h4');
+h4Text[0].textContent = siteContent["main-content"]["features-h4"];
+h4Text[1].textContent = siteContent["main-content"]["about-h4"];
+h4Text[2].textContent = siteContent["main-content"]["services-h4"];
+h4Text[3].textContent = siteContent["main-content"]["product-h4"];
+h4Text[4].textContent = siteContent['main-content']["vision-h4"];
+h4Text[5].textContent = siteContent["contact"]["contact-h4"];
+
+//p text
+let pText = document.querySelectorAll('p');
+pText[0].textContent = siteContent["main-content"]["features-content"];
+pText[1].textContent = siteContent["main-content"]["about-content"];
+pText[2].textContent = siteContent["main-content"]["services-content"];
+pText[3].textContent = siteContent["main-content"]["product-content"];
+pText[4].textContent = siteContent["main-content"]["vision-content"];
+pText[5].textContent = siteContent["contact"]["address"];
+pText[6].textContent = siteContent["contact"]["phone"];
+pText[7].textContent = siteContent["contact"]["email"];
+pText[8].textContent = siteContent["footer"]["copyright"];
